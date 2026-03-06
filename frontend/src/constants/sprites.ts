b71179470245fde5ae876e43d7f446173c02b921
Kanye-Est <keast3731@gmail.com>
2026-03-06 16:41:21 +0800

    feat: adding community

--- frontend/src/constants/sprites.ts
+++ frontend/src/constants/sprites.ts
@@ -14,6 +14,7 @@ import barImg from '@/assets/bar.png'
 import stoneImg from '@/assets/stone.png'
 import crabImg from '@/assets/crab.png'
 import shellImg from '@/assets/shell.png'
+import communityImg from '@/assets/community.png'
 
 export const SPRITES: SpriteData[] = [
 
@@ -26,8 +27,9 @@ export const SPRITES: SpriteData[] = [
   { id: 'crab',  src: crabImg,  left: '60%', top: '12%', width: '6vw' },
 
   { id: 'shell1', src: shellImg, left: '48%', top: '45%', width: '6vw',   rotate: 15 },
-  { id: 'shell2', src: shellImg, left: '56%', top: '60%', width: '5.5vw', rotate: -10, scaleX: -1 },
   { id: 'shell3', src: shellImg, left: '50%', top: '75%', width: '5vw',   rotate: 45,  scaleY: 1 },
   { id: 'shell4', src: shellImg, left: '52.5%', top: '48%', width: '5.2vw', rotate: 30, scaleX: -1, scaleY: -1 },
+
+  { id: 'community', src: communityImg, left: '54%', top: '15%', width: '45vw' },
 
 ]
